1f0e83152e1876157f1b69b6bf5a3d77998c6e5d
Ken Barlow <kenbrlow@gmail.com>
2020-08-28 01:12:59 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  count = 0
+  while count < array.length
+    yield array[0]
+    count += 1
+  end
+end
+
+my_each([1, 2, 3, 4]) do |item|
+  puts item
 end
